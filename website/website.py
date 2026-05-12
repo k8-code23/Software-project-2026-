6687bcb0ae47eaea6ca040a0e73ababe789bba8d
K8 <kate.cochon@education.nsw.gov.au>
2026-05-12 22:03:16 +1000

    update index.html file

--- website/website.py
+++ website/website.py
@@ -4,7 +4,7 @@ from pathlib import Path
 app = Flask(__name__, static_folder='.')
 
 @app.route('/')
-def home():
+def home():                 
     return send_from_directory(Path(__file__).parent, 'index.html')
 
 if __name__ == '__main__':
