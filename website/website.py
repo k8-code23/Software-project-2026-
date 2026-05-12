a8472229429c0a5c81fe1352327a17b348a9b362
K8 <kate.cochon@education.nsw.gov.au>
2026-05-12 22:55:28 +1000

    comments

--- website/website.py
+++ website/website.py
@@ -1,11 +1,2 @@
-from flask import Flask, send_from_directory
-from pathlib import Path
-
-app = Flask(__name__, static_folder='.')
-
-@app.route('/')
-def home():                 
-    return send_from_directory(Path(__file__).parent, 'index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+# beura of meterorological data and beach conditions
+#
